554129e172f6cce88eb0dd030ed5c31fd9e9a6cd
Johan Levin <johan13@gmail.com>
2022-11-30 21:49:40 +0100

    Skeleton project

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Test](https://github.com/johan13/AdventOfCode2022/actions/workflows/test.yml/badge.svg)](https://github.com/johan13/AdventOfCode2022/actions/workflows/test.yml)
+
+# Solutions to Advent of Code 2022
---
+++ src/common/fileParser.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+export default function fileParser<T>(
+    parser: (s: string, i: number) => T,
+    separator: string | RegExp = "\n",
+) {
+    return (filePath: string) =>
+        readFileSync(resolve(__dirname, "..", filePath), "utf8")
+            .trimEnd()
+            .split(separator)
+            .map(parser);
+}
